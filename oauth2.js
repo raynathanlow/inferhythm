10d9f238fa5fecd03ecf8dffc6de4b5039144376
Ray Nathan Low <raynathanlow@gmail.com>
2019-09-15 20:12:36 -0700

    Add more security to cookies
    Add secure, httpOnly, and sameSite parameters to cookies to prevent
    cross-site scripting and cross-site request forgery attacks.
    
    Some callback functions have been modified to be arrow functions to add
    more consistency.

--- oauth2.js
+++ oauth2.js
@@ -6,6 +6,16 @@ let clientSecret = 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX';
 let responseUrl, accessToken, refreshToken;
 
 window.onload = function() {
+
+  // get accessToken cookie
+  chrome.cookies.get({
+    url: 'https://accounts.spotify.com/api/token',
+    name: 'accessToken'
+  }, cookieArray => {
+    accessToken = cookieArray.value
+    console.log(accessToken);
+  });
+
   document.querySelector('#log-in').addEventListener('click', function() {
     console.log('log-in');
     // 1. request application request authorization
@@ -18,31 +28,30 @@ window.onload = function() {
       '&scope=user-read-currently-playing' + 
       '&state=' + sentState, 
       interactive: true 
-    },
-      function(response) {
-        // get the authorization code from the response
-        // https://stackoverflow.com/questions/979975/how-to-get-the-value-from-the-get-parameters
-        // URL() constructor returns a URL object which represents the URL 
-        // defined by its parameters
-        responseUrl = new URL(response);
-        code = responseUrl.searchParams.get('code');
-        // URLSearchParams: an interface with utility methods that work with
-        // the query string of a URL
-        // A query string is the part of the URL that stores data which can be
-        // passed to and from a web application and a server.
-        // The query string follows the question mark sign in the URL
+    }, response => {
+      // get the authorization code from the response
+      // https://stackoverflow.com/questions/979975/how-to-get-the-value-from-the-get-parameters
+      // URL() constructor returns a URL object which represents the URL 
+      // defined by its parameters
+      responseUrl = new URL(response);
+      code = responseUrl.searchParams.get('code');
+      // URLSearchParams: an interface with utility methods that work with
+      // the query string of a URL
+      // A query string is the part of the URL that stores data which can be
+      // passed to and from a web application and a server.
+      // The query string follows the question mark sign in the URL
 
-        state = responseUrl.searchParams.get('state');
+      state = responseUrl.searchParams.get('state');
 
-        // verify sent state is the same as the response state 
-        if (state == sentState) {
-          console.log('same state');
-        } else {
-          console.log('different state');
-          console.log('sentState: ' + sentState);
-          console.log('response state: ' + state);
-        }
-      })
+      // verify sent state is the same as the response state 
+      if (state == sentState) {
+        console.log('same state');
+      } else {
+        console.log('different state');
+        console.log('sentState: ' + sentState);
+        console.log('response state: ' + state);
+      }
+    })
   });
 
   document.querySelector('#request-a-r').addEventListener('click', function() {
@@ -71,7 +80,7 @@ window.onload = function() {
         accessToken = data.access_token;
         refreshToken = data.refresh_token;
         console.log(accessToken);
-        console.log(refreshToken);
+        // console.log(refreshToken);
 
         // set accessToken cookie
         chrome.cookies.set({
@@ -79,9 +88,10 @@ window.onload = function() {
           // not the url of where you got the data
           url: 'https://accounts.spotify.com/api/token',
           name: 'accessToken',
-          value: accessToken
-        }, function(cookie) {
-          console.log('accessToken cookie set');
+          value: accessToken,
+          secure: true,
+          httpOnly: true,
+          sameSite: 'strict',
         });
 
         // set refreshToken cookie 
@@ -90,83 +100,68 @@ window.onload = function() {
           // not the url of where you got the data
           url: 'https://accounts.spotify.com/api/token',
           name: 'refreshToken',
-          value: refreshToken
-        }, function(cookie) {
-          console.log('refreshToken cookie set');
+          value: refreshToken,
+          secure: true,
+          httpOnly: true,
+          sameSite: 'strict'
         });
       });
   });
 
-    document.querySelector('#request-data').addEventListener('click', function() {
-      console.log('request-data');
-      fetch('https://api.spotify.com/v1/me/player/currently-playing',{ 
-        method: 'GET',
-        headers: {
-          'Authorization': 'Bearer ' + accessToken
-        }
-      })
-        .then(response => response.json())
-      // print name of user's currently playing song
-        .then(data => {
-          track = data.item.name;
-          artist = data.item.artists[0].name;
+  document.querySelector('#request-data').addEventListener('click', function() {
+    console.log('request-data');
+    fetch('https://api.spotify.com/v1/me/player/currently-playing',{ 
+      method: 'GET',
+      headers: {
+        'Authorization': 'Bearer ' + accessToken
+      }
+    })
+    // check that a valid token was used
+      .then(response => response.json())
+    // print name of user's currently playing song
+      .then(data => {
+        track = data.item.name;
+        artist = data.item.artists[0].name;
 
-          // replace spaces with dashes
-          track = track.replace(/\s/g, '-');
-          artist = artist.replace(/\s/g, '-');
+        // replace spaces with dashes
+        track = track.replace(/\s/g, '-');
+        artist = artist.replace(/\s/g, '-');
 
-          url = `https://genius.com/${artist}-${track}-lyrics`;
+        url = `https://genius.com/${artist}-${track}-lyrics`;
 
-          chrome.tabs.create({url: url});
+        chrome.tabs.create({url: url});
 
-          // get accessToken cookie
-          chrome.cookies.get({
-            url: 'https://accounts.spotify.com/api/token',
-            name: 'accessToken'
-          }, function(cookieArray) {
-            console.log(cookieArray.value);
-          });
+        // use this if I want to get the featured artists
+        // for (let artist of data.item.artists) {
+        //   console.log(artist.name);
+        // }
+      });
+  });
 
-          // get refreshToken cookie
-          chrome.cookies.get({
-            url: 'https://accounts.spotify.com/api/token',
-            name: 'refreshToken'
-          }, function(cookieArray) {
-            console.log(cookieArray.value);
-          });
-
-          // use this if I want to get the featured artists
-          // for (let artist of data.item.artists) {
-          //   console.log(artist.name);
-          // }
-        });
-    });
-
-    document.querySelector('#request-r').addEventListener('click', function() {
-      console.log('request-r');
-      fetch('https://accounts.spotify.com/api/token',{ 
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/x-www-form-urlencoded',
-          'Authorization': 'Basic ' + btoa(clientId + ':' + clientSecret)
-          // btoa() encodes string in base-64
-        },
-        body: 'grant_type=refresh_token&refresh_token=' + refreshToken
-        // body: 'grant_type=authorization_code&code=' + code + '&redirect_uri=' + redirectUri + '&scope=user-read-currently-playing'
-      })
+  document.querySelector('#request-r').addEventListener('click', function() {
+    fetch('https://accounts.spotify.com/api/token',{ 
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'Authorization': 'Basic ' + btoa(clientId + ':' + clientSecret)
+        // btoa() encodes string in base-64
+      },
+      body: 'grant_type=refresh_token&refresh_token=' + refreshToken
+      // body: 'grant_type=authorization_code&code=' + code + '&redirect_uri=' + redirectUri + '&scope=user-read-currently-playing'
+    })
       .then(response => response.json())
       .then(data => accessToken = data.access_token);
-      console.log(accessToken);
-    });
+    console.log(accessToken);
+  });
 };
 
 // https://stackoverflow.com/a/1349426
 function randStr(length) {
-   var result           = '';
-   var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-   var charactersLength = characters.length;
-   for ( var i = 0; i < length; i++ ) {
-      result += characters.charAt(Math.floor(Math.random() * charactersLength));
-   }
-   return result;
+  var result           = '';
+  var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  var charactersLength = characters.length;
+  for ( var i = 0; i < length; i++ ) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
 }
